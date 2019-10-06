c2f6e9f1a2d427fe481846bd174546f48fea2fef
Alex Edward Ball <ballalexedward@gmail.com>
2019-10-06 13:53:48 -0700

    got the home quiz page showing up

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -10,6 +11,82 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Hello'));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('OpenTrivia'),
+          elevation: 0,
+        ),
+        body: Stack(
+          children: <Widget>[
+            ClipPath(
+              clipper: WaveClipperOne(),
+              child: Container(
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
+                height: 200,
+              ),
+            ),
+            CustomScrollView(
+              physics: BouncingScrollPhysics(),
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    child: Text(
+                      "Select a category to start the quiz",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16.0),
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.all(16.0),
+                  sliver: SliverGrid(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          childAspectRatio: 1.2,
+                          crossAxisSpacing: 10.0,
+                          mainAxisSpacing: 10.0),
+                      delegate: SliverChildBuilderDelegate(
+                        _buildCategoryItem,
+                        childCount: 1,
+                      )),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildCategoryItem(BuildContext context, int index) {
+    return MaterialButton(
+      elevation: 1.0,
+      highlightElevation: 1.0,
+      onPressed: () => _categoryPressed(context),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      color: Colors.grey.shade800,
+      textColor: Colors.white70,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Hello'),
+        ],
+      ),
+    );
+  }
+
+  _categoryPressed(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (sheetContext) => BottomSheet(
+        builder: (_) => Text('Bottom Sheet'),
+        onClosing: () {},
+      ),
+    );
   }
 }
