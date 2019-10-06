cb114db1fc7f25bf4dd2e6aceaf17b8255634718
Alex Edward Ball <ballalexedward@gmail.com>
2019-10-06 14:23:02 -0700

    trying to wire up api

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,61 +12,42 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Mindful Philanthopy'),
-          elevation: 0,
-        ),
-        body: Stack(
-          children: <Widget>[
-            ClipPath(
-              clipper: WaveClipperOne(),
-              child: Container(
-                decoration:
-                    BoxDecoration(color: Theme.of(context).primaryColor),
-                height: 100,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 20.0),
-              width: 100,
-              height: 100,
-              child: MaterialButton(
-                elevation: 1.0,
-                highlightElevation: 1.0,
-                onPressed: () => _categoryPressed(context),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                color: Theme.of(context).accentColor,
-                textColor: Colors.white70,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Donate to a charity',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ));
-  }
-
-  Widget _buildCategoryItem(BuildContext context, int index) {
-    return MaterialButton(
-      elevation: 1.0,
-      highlightElevation: 1.0,
-      onPressed: () => _categoryPressed(context),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
+      appBar: AppBar(
+        title: Text('Mindful Philanthopy'),
+        elevation: 0,
       ),
-      color: Colors.grey.shade800,
-      textColor: Colors.white70,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          Text('Hello'),
+          ClipPath(
+            clipper: WaveClipperOne(),
+            child: Container(
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+              height: 100,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20.0),
+            width: 100,
+            height: 100,
+            child: MaterialButton(
+              elevation: 1.0,
+              highlightElevation: 1.0,
+              onPressed: () => _categoryPressed(context),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              color: Theme.of(context).accentColor,
+              textColor: Colors.white70,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Donate to a charity',
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
