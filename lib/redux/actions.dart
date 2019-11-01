274f2d6f540aa5a4e4c65107a9be4ec421cc49ec
Alex Edward Ball <ballalexedward@gmail.com>
2019-10-31 17:46:59 -0700

    added actions and reducers

---
+++ lib/redux/actions.dart
@@ -0,0 +1,5 @@
+class Processing {
+  final bool payload;
+
+  Processing(this.payload);
+}
---
+++ lib/redux/reducers.dart
@@ -0,0 +1,12 @@
+import '../store/app_state.dart';
+import './actions.dart';
+
+AppState reducer(AppState prevState, dynamic action) {
+  AppState newState = AppState.fromAppState(prevState);
+
+  if (action is Processing) {
+    newState.processing = action.payload;
+  }
+
+  return newState;
+}
