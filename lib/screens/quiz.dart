b4531742f0be1f1cfc353e8cff75080263d5dbac
Alex Edward Ball <ballalexedward@gmail.com>
2019-11-09 13:37:25 -0800

    style(quizScreen): style the quiz screen according to mockups

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -56,75 +56,83 @@ class QuizPage extends StatelessWidget {
           title: Text('Philantropy Questions'),
           elevation: 0,
         ),
-        body: Stack(
-          children: <Widget>[
-            ClipPath(
-              clipper: WaveClipperOne(),
-              child: Container(
-                decoration:
-                    BoxDecoration(color: Theme.of(context).primaryColor),
-                height: 200,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white70,
-                        child: Text("${state.quizState.currentIndex + 1}"),
-                      ),
-                      SizedBox(width: 16.0),
-                      Expanded(
-                        child: Text(
-                            state
-                                .quizState
-                                .questions[state.quizState.currentIndex]
-                                .question,
-                            style: kQuestionStyle),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20.0),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: state
-                        .quizState.options[state.quizState.currentIndex].length,
-                    itemBuilder: (context, idx) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: state.quizState.colors[idx],
-                          border: Border.all(width: 1.0),
+        body: Container(
+          decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1.0),
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: kCard),
+                margin: EdgeInsets.fromLTRB(15.0, 40.0, 15.0, 90.0),
+                padding: EdgeInsets.all(16.0),
+                // height: 450,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.white70,
+                          child: Text("${state.quizState.currentIndex + 1}"),
                         ),
-                        child: ListTile(
-                          title: Text(state.quizState
-                              .options[state.quizState.currentIndex][idx]),
-                          onTap: () {
-                            StoreProvider.of<AppState>(context)
-                                .dispatch(ChangeSelectedColor(index: idx));
-
-                            StoreProvider.of<AppState>(context)
-                                .dispatch(oneSecondDelay());
-
-                            StoreProvider.of<AppState>(context).dispatch(
-                                AddAnswer(
-                                    selection: state.quizState.options[
-                                        state.quizState.currentIndex][idx]));
-
-                            StoreProvider.of<AppState>(context)
-                                .dispatch(resetQuestionState());
-                            // _nextSubmit(context);
-                          },
+                        SizedBox(width: 16.0),
+                        Expanded(
+                          child: Text(
+                              state
+                                  .quizState
+                                  .questions[state.quizState.currentIndex]
+                                  .question,
+                              style: kQuestionStyle),
                         ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                      ],
+                    ),
+                    SizedBox(height: 20.0),
+                    ListView.separated(
+                      shrinkWrap: true,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: 16,
+                        );
+                      },
+                      itemCount: state.quizState
+                          .options[state.quizState.currentIndex].length,
+                      itemBuilder: (context, idx) {
+                        return Container(
+                          decoration: BoxDecoration(
+                              color: state.quizState.colors[idx],
+                              borderRadius: BorderRadius.circular(4.0)),
+                          child: ListTile(
+                            title: Text(state.quizState
+                                .options[state.quizState.currentIndex][idx]),
+                            onTap: () {
+                              StoreProvider.of<AppState>(context)
+                                  .dispatch(ChangeSelectedColor(index: idx));
+
+                              StoreProvider.of<AppState>(context)
+                                  .dispatch(oneSecondDelay());
+
+                              StoreProvider.of<AppState>(context).dispatch(
+                                  AddAnswer(
+                                      selection: state.quizState.options[
+                                          state.quizState.currentIndex][idx]));
+
+                              StoreProvider.of<AppState>(context)
+                                  .dispatch(resetQuestionState());
+                              // _nextSubmit(context);
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
